599567472bbfa6f29406c70476c8d79d1af6f572
E.M <ezequiel.macciuci@gmail.com>
2022-05-10 18:04:52 +0200

    correccion para sumar funcion

--- desafio1.js
+++ desafio1.js
@@ -6,6 +6,30 @@ const PRECIO_RISOTTO = 14;
 let total = 0;
 let vuelveAPreguntar = true;
 
+function sumarMenu(articulo){
+   
+  if (articulo == 1) {
+    total = total + PRECIO_HAMBURGUESA;
+    console.log("SUMASTE HAMBURGUESA");
+  } else if (articulo == 2) {
+    total = total + PRECIO_RAVIOLES;
+    console.log("SUMASTE RAVIOLES");
+  } else if (articulo == 3) {
+    total = total + PRECIO_VITTELTONNE;
+    console.log("SUMASTE VITTEL TONNE");
+  } else if (articulo == 4) {
+    total = total + PRECIO_RISOTTO;
+    console.log("SUMASTE RISOTTO");
+  } else if (articulo == 0) {
+    vuelveAPreguntar = false;
+  } else {
+    alert("Valor invalido");
+  }
+  alert("el total es: " + total);
+
+}
+
+
 while (vuelveAPreguntar) {
   let mensaje =
     "INGRESAR LO QUE QUIERAS COMER Y TOCA 0 PARA FINALIZAR Y VER EL TOTAL  MENU  1.Hamburguesa  2.Ravioles  3.Vittel Tonne  4. Risotto";
@@ -13,23 +37,8 @@ while (vuelveAPreguntar) {
     mensaje =
       "INGRESAR SIGUIENTE PRODUCTO TOCA 0 PARA FINALIZAR Y VER EL TOTAL  MENU  1.Hamburguesa  2.Ravioles  3.Vittel Tonne  4. Risotto";
   }
-  let entrada1 = prompt(mensaje);
-  if (entrada1 == 1) {
-    total = total + PRECIO_HAMBURGUESA;
-    console.log("SUMASTE HAMBURGUESA");
-  } else if (entrada1 == 2) {
-    total = total + PRECIO_RAVIOLES;
-    console.log("SUMASTE RAVIOLES");
-  } else if (entrada1 == 3) {
-    total = total + PRECIO_VITTELTONNE;
-    console.log("SUMASTE VITTEL TONNE");
-  } else if (entrada1 == 4) {
-    total = total + PRECIO_RISOTTO;
-    console.log("SUMASTE RISOTTO");
-  } else if (entrada1 == 0) {
-    vuelveAPreguntar = false;
-  } else {
-    alert("Valor invalido");
-  }
+  
+  sumarMenu(prompt(mensaje))
 }
-alert("el total es: " + total);
+
+
